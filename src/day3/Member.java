ad17d187b9ca7f8f3c056f8c32c45f467a940105
Minzino <gurumcider@gmail.com>
2021-11-23 14:19:19 +0900

    Hashset을 사용해서 랜덤변수 중복제거

---
+++ src/day3/Member.java
@@ -0,0 +1,31 @@
+package day3;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Random;
+import java.util.Set;
+
+public class Member {
+	public static void randomPrint(int number) {
+
+		Set<Integer> set = new HashSet<>(); //HashSet을 이용하여 랜덤함수의 중복제거
+
+		Random random = new Random();
+		StringBuilder sb = new StringBuilder();
+		String[] member = { "김태수", "정상준", "송민제", "강승우", "이준선", "김민욱", "송형근", "김경민" };
+		while (set.size() < number) {
+			set.add(random.nextInt(member.length));
+		}
+		
+		Iterator<Integer> randomNumber = set.iterator();
+		while (randomNumber.hasNext()) {
+			if (number == 1) {
+				sb.append(member[randomNumber.next()]);
+				break;
+			}
+			sb.append(member[randomNumber.next()] + ",");
+			number--;
+		}
+		System.out.println(sb);
+	}
+}
--- src/day3/RandomName.java
+++ src/day3/RandomName.java
@@ -1,39 +1,26 @@
 package day3;
 
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.Set;
 
 public class RandomName {
 	/*
-	 * to do list - 랜덤함수를 이용함. 
-	 * - 만약에 사람 이름이 중복이 된다면 다시 랜덤함수를 통해 다른 사람을 뽑음 
-	 * - 마지막 사람 이름 뒤에는 , 표시를 하지 않음 
-	 * 지정한 숫자만큼 코코아 멤버들의 이름을 랜덤 출력한다. 
-	 * 몇 명의 멤버를 출력할까요? 5 
-	 * BC, Who, 피오, 노리, Jun
+	 * to do list - 랜덤함수를 이용함. - 만약에 사람 이름이 중복이 된다면 다시 랜덤함수를 통해 다른 사람을 뽑음 - 마지막 사람
+	 * 이름 뒤에는 , 표시를 하지 않음 지정한 숫자만큼 코코아 멤버들의 이름을 랜덤 출력한다. 몇 명의 멤버를 출력할까요? 5 BC, Who,
+	 * 피오, 노리, Jun
 	 * 
 	 */
-	public static void memberPrint(int number) {
-
-		Random random = new Random();
-		StringBuilder sb = new StringBuilder();
-		String[] member = { "김태수", "정상준", "송민제", "강승우", "이준선", "김민욱", "송형근", "김경민" };
-		do {
-			sb.append(member[random.nextInt(member.length)] + ",");
-			number--;
-		} while (number > 1);
-		{
-			sb.append(member[random.nextInt(member.length)]);
-		}
-		System.out.println(sb);
-	}
-
 	public static void main(String args[]) {
+		Member member = new Member();
 		Scanner sc = new Scanner(System.in);
 		System.out.print("몇 명의 멤버를 출력할까요?");
 		int number = sc.nextInt();
+		sc.close();
 
-		memberPrint(number);
+		member.randomPrint(number);
 
 	}
 }
